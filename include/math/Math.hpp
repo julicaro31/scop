d59021f85325388dcf0ea1427bfb9665289d4ac4
Julia Caro <juliac.3195@gmail.com>
2026-03-22 12:27:52 +0100

    added vec3 implementation

---
+++ include/math/Math.hpp
@@ -0,0 +1,2 @@
+#pragma once
+#include "Vec3.hpp"
---
+++ include/math/Vec3.hpp
@@ -0,0 +1,51 @@
+#pragma once
+#include <cmath>
+
+struct Vec3
+{
+	float x, y, z;
+
+	Vec3() : x(0), y(0), z(0) {}
+	Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
+
+	Vec3 operator+(const Vec3 &v) const
+	{
+		return Vec3(x + v.x, y + v.y, z + v.z);
+	}
+
+	Vec3 operator-(const Vec3 &v) const
+	{
+		return Vec3(x - v.x, y - v.y, z - v.z);
+	}
+
+	Vec3 operator*(float s) const
+	{
+		return Vec3(x * s, y * s, z * s);
+	}
+
+	float dot(const Vec3 &v) const
+	{
+		return x * v.x + y * v.y + z * v.z;
+	}
+
+	Vec3 cross(const Vec3 &v) const
+	{
+		return Vec3(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
+	}
+
+	float length() const
+	{
+		return std::sqrt(x * x + y * y + z * z);
+	}
+
+	void normalize()
+	{
+		float len = length();
+		if (len > 0.0f)
+		{
+			x /= len;
+			y /= len;
+			z /= len;
+		}
+	}
+};
